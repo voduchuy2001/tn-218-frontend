2ca4e4cad5588b843841ae6638b8abd15866b13d
MinionB1909989 <B1909989quyetthang@gmail.com>
2023-03-01 14:44:10 +0700

    success list cart

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,62 +6,61 @@ import { useSelector } from "react-redux";
 import { decrement } from "../../features/counter/counterSlice";
 import { useDispatch } from "react-redux";
 const activeClass = ({ isActive }) => {
-    return isActive
-        ? `bg-primary-color hover:cursor-pointer p-2 text-white`
-        : `hover:bg-primary-color hover:opacity-80 hover:text-white hover:cursor-pointer p-2`;
+  return isActive
+    ? `bg-primary-color hover:cursor-pointer p-2 text-white`
+    : `hover:bg-primary-color hover:opacity-80 hover:text-white hover:cursor-pointer p-2`;
 };
 
 const Navbar = () => {
-    const [listCart, setListCart] = useState([]);
-    const dispatch=useDispatch()
-    const handleDelete=(idProduct)=>(
-      axios
-          .delete(`http://127.0.0.1:8000/api/cart/${idProduct}`, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-          })
-          .then((res) => {
-            dispatch(decrement())
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(err);
-          })
-    )
-    useEffect(() => {
-        axios
-          .get("http://127.0.0.1:8000/api/cart", {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-          })
-          .then((res) => {
-            console.log(res.data.data.items);
-            setListCart(res.data.data.items);
-          })
-          .catch((err) => {
-            console.log(err);
-            setListCart([]);
-          });
-      }, [useSelector(selectCount)]);
-      
-    return (
-        <nav className="h-11 bg-secondary-color mobile:hidden tablet:hidden">
-            <div className="grids wide">
-                <div className="h-11 flex justify-between items-center font-semibold">
-                    <div className="flex h-11">
-                        <NavLink className={activeClass} to="/">
-                            Trang chủ
-                        </NavLink>
-                        <NavLink className={activeClass} to="/products">
-                            <p>Sản phẩm</p>
-                        </NavLink>
-                        <NavLink className={activeClass} to="/orders">
-                            Đơn hàng
-                        </NavLink>
-                    </div>
-                    {localStorage.getItem("user") ? (
+  const [listCart, setListCart] = useState([]);
+  const dispatch = useDispatch();
+  const handleDelete = (idProduct) =>
+    axios
+      .delete(`http://127.0.0.1:8000/api/cart/${idProduct}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((res) => {
+        dispatch(decrement());
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/cart", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.data.items);
+        setListCart(res.data.data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+        setListCart([]);
+      });
+  }, [useSelector(selectCount)]);
+
+  return (
+    <nav className="h-11 bg-secondary-color mobile:hidden tablet:hidden">
+      <div className="grids wide">
+        <div className="h-11 flex justify-between items-center font-semibold">
+          <div className="flex h-11">
+            <NavLink className={activeClass} to="/">
+              Trang chủ
+            </NavLink>
+            <NavLink className={activeClass} to="/products">
+              <p>Sản phẩm</p>
+            </NavLink>
+            <NavLink className={activeClass} to="/orders">
+              Đơn hàng
+            </NavLink>
+          </div>
+          {localStorage.getItem("user") ? (
             <div className="group relative  hover:cursor-pointer font-semibold bg-primary-color p-[4px_12px] rounded-lg">
               {/* Cart */}
               <div className="flex gap-2 justify-center items-center">
@@ -95,71 +94,77 @@ const Navbar = () => {
                   </div>
                 ) : (
                   <ul className="max-h-[370px] overflow-y-scroll">
-                    
-                    {listCart.map((item)=>(
-                      <li key={item.id} className="item-cart hover:bg-slate-100 flex align-center">
-                      <img
-                        className="m-3 w-[40px] h-[40px] border-solid border-[1px] border-[#e8e8e8]"
-                        src={item.product.image}
-                        alt="IMAGE"
-                      />
-                      <div className="w-[100%] mr-3">
-                        <div className="flex justify-between">
-                          <h5>{item.product.name}</h5>
-                          <div>
-                            <span className="text-primary-color">
-                              {item.product.price}
-                              <span className="align-top text-[12px]">
-                                {" "}
-                                USD
+                    {listCart.map((item) => (
+                      <li
+                        key={item.id}
+                        className="item-cart hover:bg-slate-100 flex align-center"
+                      >
+                        <img
+                          className="m-3 w-[40px] h-[40px] border-solid border-[1px] border-[#e8e8e8]"
+                          src={item.product.image}
+                          alt="IMAGE"
+                        />
+                        <div className="w-[100%] mr-3">
+                          <div className="flex justify-between">
+                            <h5>{item.product.name}</h5>
+                            <div>
+                              <span className="text-primary-color">
+                                {item.product.price}
+                                <span className="align-top text-[12px]">
+                                  {" "}
+                                  USD
+                                </span>
                               </span>
+                              <span className="text-[10px] text-[#757575]">
+                                x
+                              </span>
+                              <span className="text-[12px] text-[#757575]">
+                                {item.quantity}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="flex justify-between">
+                            <span className="text-[14px] text-[#757575] font-[300]">
+                              Phân loại: VIP
                             </span>
-                            <span className="text-[10px] text-[#757575]">
-                              x
-                            </span>
-                            <span className="text-[12px] text-[#757575]">
-                              {item.quantity}
+
+                            <span
+                              onClick={() => {
+                                handleDelete(item.product_id);
+                              }}
+                              className="hover:text-primary-color text-[14px] text-[#757575]"
+                            >
+                              Xoá
                             </span>
                           </div>
                         </div>
-                        <div className="flex justify-between">
-                          <span className="text-[14px] text-[#757575] font-[300]">
-                            Phân loại: VIP
-                          </span>
-                          
-                            <span onClick={() => {
-                        handleDelete(item.product_id);
-                      }} className="hover:text-primary-color text-[14px] text-[#757575]">
-                              Xoá
-                            </span>
-                          
-                        </div>
-                      </div>
-                    </li>
+                      </li>
                     ))}
                   </ul>
                 )}
 
                 {/* Has carts */}
-
-                <div className="p-2 text-right cursor-default">
-                                <Link
-                                    to="/cart"
-                                    className="hover:opacity-90 font-semibold text-[12px] p-[8px_12px] bg-primary-color text-white"
-                                >
-                                    Xem giỏ hàng
-                                </Link>
-                            </div>
-                            
+                {listCart.length === 0 ? (
+                  <></>
+                ) : (
+                  <div className="p-2 text-right cursor-default">
+                    <Link
+                      to="/cart"
+                      className="hover:opacity-90 font-semibold text-[12px] p-[8px_12px] bg-primary-color text-white"
+                    >
+                      Xem giỏ hàng
+                    </Link>
+                  </div>
+                )}
               </div>
             </div>
           ) : (
             <></>
           )}
-                </div>
-            </div>
-        </nav>
-    );
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
